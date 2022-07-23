b3dbf1671e8de9edf803e98de1e543ae0cee8d8b
vasyaboychuk <boychuk98vasya@gmail.com>
2022-07-23 11:48:41 +0300

    add lesson1 js

---
+++ lesson1/main.js
@@ -0,0 +1,90 @@
+let str1='hello';
+let str2='owu';
+let str3='com';
+let str4='ua';
+let n1=1;
+let n2=10;
+let n3=-999;
+let n4=123;
+let n5=3.14;
+let n6=2.7;
+let n7=16;
+let b1=true;
+let b2=false;
+console.log(str1);
+alert(str1);
+document.write(str1);
+
+console.log(str2);
+alert(str2);
+document.write(str2);
+
+console.log(str3);
+alert(str3);
+document.write(str3);
+
+console.log(str4);
+alert(str4);
+document.write(str4);
+
+console.log(n1);
+alert(n1);
+document.write(n1);
+
+console.log(n2);
+alert(n2);
+document.write(n2);
+
+console.log(n3);
+alert(n3);
+document.write(n3);
+
+console.log(n4);
+alert(n4);
+document.write(n4);
+
+console.log(n5);
+alert(n5);
+document.write(n5);
+
+console.log(n6);
+alert(n6);
+document.write(n6);
+
+console.log(n7);
+alert(n7);
+document.write(n7);
+
+console.log(b1);
+alert(b1);
+document.write(b1);
+
+console.log(b2);
+alert(b2);
+document.write(b2);
+
+
+
+let  firstName='Василь';
+let middleName=' Васильович';
+let lastName=' Бойчук';
+let person=firstName+middleName+lastName;
+console.log(person);
+
+
+
+
+let a=100;
+let b='100';
+let c=true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+let firstName2= prompt('Your Name?');
+console.log(firstName2);
+let middleName2=prompt('Your Middle Name?');
+console.log(middleName2);
+let Age=+prompt('How old are you?')
+console.log(Age)

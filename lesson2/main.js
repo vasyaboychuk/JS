b35d33c6f79d5ab84e7c52f0179e68e12da62aa9
vasyaboychuk <boychuk98vasya@gmail.com>
2022-07-27 14:24:09 +0300

    add lesson2 Js

---
+++ lesson2/main.js
@@ -0,0 +1,67 @@
+let producers =['BMW','AUDI','VOLVO','WV','FORD','MERCEDES','OPEL','HONDA','NISSAN','SKODA']
+console.log(producers[0]);
+console.log(producers[1]);
+console.log(producers[2]);
+console.log(producers[3]);
+console.log(producers[4]);
+console.log(producers[5]);
+console.log(producers[6]);
+console.log(producers[7]);
+console.log(producers[8]);
+console.log(producers[9]);
+console.log(producers[10]);
+
+
+let book1 = {title: 'Harry Potter', pageCount: 500, genre: 'Fantasy'};
+let book2 = {title: "Da Vinci code", pageCount: 400, genre: "detective"};
+let book3 = {title: 'Silkworm', pageCount: 600, genre: 'detective'};
+
+
+
+let book4 = {
+    title: 'Hobbit',
+    pageCount: 400,
+    genre: 'Fantasy',
+    author:[{name:'Peter Jackson'},{age:60}]};
+
+let book5 = {
+    title: "Angels and demons",
+    pageCount: 450,
+    genre: "detective",
+    author:[{name:'Dan Brown'},{age:50}]};
+let book6 = {
+    title: 'Murder on the Nil',
+    pageCount: 300,
+    genre: 'detective',
+    author:[{name:'Agata Christy'},{age:'died'}]};
+
+
+
+
+
+let users=[
+    {name:'vasyl',username:'vasya1',password:'vasya123'},
+    {name:'oleg',username:'oleg2',password: 'oleg1234'},
+    {name:'nastya',username: 'nastya',password:123456},
+    {name:'petro',username: 'petya2',password: 'petro6543'},
+    {name: 'dmytro',username:'dima1',password:'dimaqwerty'},
+    {name: 'bogdan',username: 'bodya135',password: 'bodya135'},
+    {name: 'olya',username: 'olyya1',password: 'olya1234'},
+    {name: 'julia',username: 'iamjulia',password: 'julia654321'},
+    {name: 'tanya',username: 'tetiana',password: 'teddy1'},
+    {name: 'yurii',username: 'yudzhin',password: 'yura1356'},
+]
+console.log(users[0].password);
+console.log(users[1]['password']);
+console.log(users[2].password);
+console.log(users[3]['password']);
+console.log(users[4].password);
+console.log(users[5]['password']);
+console.log(users[6]['password']);
+console.log(users[7].password);
+console.log(users[8]['password']);
+console.log(users[9].password);
+
+
+
+
